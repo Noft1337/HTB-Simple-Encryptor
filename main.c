89b9dbc161bd51713532eae6b5578411e0b8988f
user <user@localhost.localdomain>
2023-05-20 16:17:46 +0300

    Finished

--- main.c
+++ main.c
@@ -17,26 +17,6 @@ unsigned char unshift(unsigned char c, signed char to_shift){
     return c;
 }
 
-unsigned char decode_char(char c, int* rands, int position){
-    int rand1 = (unsigned char)rands[position];
-    int rand2 = (char)rands[position + 1];
-    c = unshift(c, rand2);
-    c = c ^ rand1;
-    return c;
-}
-
-void decode_string(unsigned char *content, int size, int *rands, int start_pos){
-    int rand1, rand2;
-    int j = 0;
-    for (int i = start_pos; i < size; i++){
-        content[i] = decode_char(content[i], rands, j);
-        printf("%c", content[i]);
-        j += 2;
-    }
-    printf("\n");
-    return;
-}
-
 void init_rand_int_array(int seed, int* array){
     srand(seed);
     for (int i = 0; i < 100; i+=2){
@@ -46,69 +26,42 @@ void init_rand_int_array(int seed, int* array){
     return;
 }
 
-int validate_decoding(char c_to_decode, char expected, int *randoms, int position){
-    if (decode_char(c_to_decode, randoms, position) == expected){
-        return 1;
-    }
-    else {
-        return 0;
-    }
+int get_seed(FILE *file){ 
+    unsigned int final_seed;
+    fread(&final_seed, sizeof(unsigned int), 1, file);
+    return final_seed;
 }
 
-int get_seed(unsigned char c1, unsigned char c2, unsigned char c3, int seed){
-    char decoded_char, decoded_char2;
-    int randoms[100];
-    unsigned int final_seed = 0;
-    for (unsigned int i = seed - 100000; i < seed + 100000; i++){
-        srand(i);
-        init_rand_int_array(i, randoms);
-        if (validate_decoding(c1, 'H', randoms, 0)) {
-            if (validate_decoding(c2, 'T', randoms, 2)){
-                if (validate_decoding(c3, 'B', randoms, 6)){
-                    final_seed = i;
-                    break;
-                }
-            }
-        }
+void decode2(unsigned char *content, int size, unsigned int seed){
+    unsigned char r1;
+    char r2;
+    unsigned char current_c;
+    printf("Flag: ");
+    srand(seed);
+    for (int i = 4; i < size; i++){
+        r1 = (unsigned char)rand();
+        r2 = (char)rand() & 7;
+        current_c = unshift(content[i], r2);
+        current_c = current_c ^ r1;
+        printf("%c", current_c);
     }
-
-    if (final_seed == 0){
-        return 0;
-    }
-
-    return final_seed;
+    printf("\n");
+    return;
 }
 
 int main(){
 
-    // I think this is the correct seed but it doesn't work for me... 
-    int seed = 1658229288;
-    int found_seed = 0;
-    int start_pos;
     FILE *inputFile = fopen("flag.enc", "rb");
+    unsigned int seed = get_seed(inputFile);
     int size = get_file_size(inputFile);
     unsigned char *content = (unsigned char *)malloc(size);
     fread(content, 1, (size_t)size, inputFile);
     fclose(inputFile);
     int rand_nums[100];
 
-    for (int x = 0; x < size; x++){
-        found_seed = get_seed(content[x], content[x + 1], content[x + 2], seed);
-        if (found_seed != 0){
-            printf("Start pos: %d\n", x);
-            start_pos = x;
-            break;
-        }
-    }
-    if (found_seed != 0){
-        printf("The seed that has been found is: %d\n", found_seed);
-        init_rand_int_array(found_seed, rand_nums);
+    printf("The seed that has been found is: %d\n", seed);
+    decode2(content, size, seed);
 
-        decode_string(content, size, rand_nums, start_pos);
-        return 0;
-    } else {
-        printf("Found no matching seed...\n");
-        return 1;
-    }
+    return 0;
 
 }
